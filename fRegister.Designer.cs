47f5866aeca5aa5b57cdca2880e14366b435de46
ntmt <nguyenthanhminhtriet3402@gmail.com>
2024-03-17 17:42:40 +0700

    UI_register

--- fRegister.Designer.cs
+++ fRegister.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fRegister));
             button1 = new Button();
             button2 = new Button();
             label1 = new Label();
@@ -35,19 +36,19 @@
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
+            txtUsername = new TextBox();
+            txtEmail = new TextBox();
+            txtPass = new TextBox();
+            txtPassConfirm = new TextBox();
             textBox5 = new TextBox();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(277, 341);
+            button1.Location = new Point(66, 301);
             button1.Name = "button1";
-            button1.Size = new Size(165, 29);
+            button1.Size = new Size(88, 29);
             button1.TabIndex = 0;
             button1.Text = "Register";
             button1.UseVisualStyleBackColor = true;
@@ -55,108 +56,115 @@
             // button2
             // 
             button2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(475, 341);
+            button2.Location = new Point(251, 300);
             button2.Name = "button2";
-            button2.Size = new Size(152, 29);
+            button2.Size = new Size(84, 30);
             button2.TabIndex = 1;
-            button2.Text = "Return to Login";
+            button2.Text = "Cancel";
             button2.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(137, 102);
+            label1.Location = new Point(15, 80);
             label1.Name = "label1";
-            label1.Size = new Size(80, 20);
+            label1.Size = new Size(70, 15);
             label1.TabIndex = 2;
-            label1.Text = "Username";
+            label1.Text = "Username :";
             // 
             // label2
             // 
             label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(137, 157);
+            label2.Location = new Point(15, 135);
             label2.Name = "label2";
-            label2.Size = new Size(47, 20);
+            label2.Size = new Size(42, 15);
             label2.TabIndex = 3;
-            label2.Text = "Email";
+            label2.Text = "Email :";
             // 
             // label3
             // 
             label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
             label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(137, 216);
+            label3.Location = new Point(15, 194);
             label3.Name = "label3";
-            label3.Size = new Size(76, 20);
+            label3.Size = new Size(65, 15);
             label3.TabIndex = 4;
-            label3.Text = "Password";
+            label3.Text = "Password :";
             // 
             // label4
             // 
             label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
             label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(137, 272);
+            label4.Location = new Point(15, 250);
             label4.Name = "label4";
-            label4.Size = new Size(137, 20);
+            label4.Size = new Size(113, 15);
             label4.TabIndex = 5;
-            label4.Text = "Confirm Password";
+            label4.Text = "Confirm Password :";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 13F, FontStyle.Bold, GraphicsUnit.Point);
-            label5.Location = new Point(411, 35);
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Times New Roman", 27.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(131, 9);
             label5.Name = "label5";
-            label5.Size = new Size(98, 30);
+            label5.Size = new Size(148, 42);
             label5.TabIndex = 6;
             label5.Text = "Register";
             // 
-            // textBox1
+            // txtUsername
             // 
-            textBox1.Location = new Point(287, 99);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(349, 27);
-            textBox1.TabIndex = 7;
+            txtUsername.Location = new Point(165, 77);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(218, 23);
+            txtUsername.TabIndex = 7;
             // 
-            // textBox2
+            // txtEmail
             // 
-            textBox2.Location = new Point(287, 154);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(349, 27);
-            textBox2.TabIndex = 8;
+            txtEmail.Location = new Point(165, 132);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(218, 23);
+            txtEmail.TabIndex = 8;
             // 
-            // textBox3
+            // txtPass
             // 
-            textBox3.Location = new Point(287, 213);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(349, 27);
-            textBox3.TabIndex = 9;
+            txtPass.Location = new Point(165, 191);
+            txtPass.Name = "txtPass";
+            txtPass.Size = new Size(218, 23);
+            txtPass.TabIndex = 9;
             // 
-            // textBox4
+            // txtPassConfirm
             // 
-            textBox4.Location = new Point(287, 272);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(349, 27);
-            textBox4.TabIndex = 10;
+            txtPassConfirm.Location = new Point(165, 247);
+            txtPassConfirm.Name = "txtPassConfirm";
+            txtPassConfirm.Size = new Size(218, 23);
+            txtPassConfirm.TabIndex = 10;
             // 
             // textBox5
             // 
             textBox5.Location = new Point(524, 446);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(8, 27);
+            textBox5.Size = new Size(8, 23);
             textBox5.TabIndex = 11;
             // 
             // fRegister
             // 
-            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(900, 450);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(420, 363);
             Controls.Add(textBox5);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(txtPassConfirm);
+            Controls.Add(txtPass);
+            Controls.Add(txtEmail);
+            Controls.Add(txtUsername);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
@@ -180,10 +188,10 @@
         private Label label3;
         private Label label4;
         private Label label5;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
+        private TextBox txtUsername;
+        private TextBox txtEmail;
+        private TextBox txtPass;
+        private TextBox txtPassConfirm;
         private TextBox textBox5;
     }
 }
