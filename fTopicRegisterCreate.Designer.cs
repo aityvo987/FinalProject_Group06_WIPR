c0f1c54328644a922a372fe0014954745991149b
AnhTai124 <20110397@student.hcmute.edu.vn>
2024-03-21 00:36:34 +0700

    Tai
    topicstudent

--- fTopicRegisterCreate.Designer.cs
+++ fTopicRegisterCreate.Designer.cs
@@ -28,214 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox2 = new TextBox();
-            textBox1 = new TextBox();
-            button2 = new Button();
-            button1 = new Button();
-            label11 = new Label();
-            label9 = new Label();
-            label1 = new Label();
-            label7 = new Label();
-            label4 = new Label();
-            label3 = new Label();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            comboBox1 = new ComboBox();
-            label2 = new Label();
-            comboBox2 = new ComboBox();
-            SuspendLayout();
+            this.txtStudent2 = new System.Windows.Forms.TextBox();
+            this.txtStudent1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNametp = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtRequirements = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTypetp = new System.Windows.Forms.TextBox();
+            this.txtNameins = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
             // 
-            // textBox2
+            // txtStudent2
             // 
-            textBox2.Location = new Point(162, 438);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 34;
+            this.txtStudent2.Location = new System.Drawing.Point(162, 438);
+            this.txtStudent2.Name = "txtStudent2";
+            this.txtStudent2.Size = new System.Drawing.Size(125, 27);
+            this.txtStudent2.TabIndex = 34;
             // 
-            // textBox1
+            // txtStudent1
             // 
-            textBox1.Location = new Point(162, 389);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 33;
+            this.txtStudent1.Location = new System.Drawing.Point(162, 389);
+            this.txtStudent1.Name = "txtStudent1";
+            this.txtStudent1.Size = new System.Drawing.Size(125, 27);
+            this.txtStudent1.TabIndex = 33;
             // 
             // button2
             // 
-            button2.BackColor = Color.IndianRed;
-            button2.Location = new Point(45, 507);
-            button2.Name = "button2";
-            button2.Size = new Size(145, 29);
-            button2.TabIndex = 31;
-            button2.Text = "Cancel";
-            button2.UseVisualStyleBackColor = false;
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.Location = new System.Drawing.Point(45, 507);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 29);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = false;
             // 
-            // button1
+            // btnCreate
             // 
-            button1.BackColor = Color.LawnGreen;
-            button1.ForeColor = SystemColors.ActiveCaptionText;
-            button1.ImageAlign = ContentAlignment.TopCenter;
-            button1.Location = new Point(516, 507);
-            button1.Name = "button1";
-            button1.Size = new Size(145, 29);
-            button1.TabIndex = 32;
-            button1.Text = "Create";
-            button1.UseVisualStyleBackColor = false;
+            this.btnCreate.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnCreate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCreate.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnCreate.Location = new System.Drawing.Point(516, 507);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(145, 29);
+            this.btnCreate.TabIndex = 32;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            this.btnCreate.Click += new System.EventHandler(this.button1_Click);
             // 
             // label11
             // 
-            label11.AutoSize = true;
-            label11.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label11.Location = new Point(45, 438);
-            label11.Name = "label11";
-            label11.Size = new Size(77, 20);
-            label11.TabIndex = 29;
-            label11.Text = "Student 2";
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(45, 438);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(77, 20);
+            this.label11.TabIndex = 29;
+            this.label11.Text = "Student 2";
             // 
             // label9
             // 
-            label9.AutoSize = true;
-            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label9.Location = new Point(45, 389);
-            label9.Name = "label9";
-            label9.Size = new Size(77, 20);
-            label9.TabIndex = 30;
-            label9.Text = "Student 1";
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(45, 389);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 20);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Student 1";
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(396, 104);
-            label1.Name = "label1";
-            label1.Size = new Size(79, 20);
-            label1.TabIndex = 25;
-            label1.Text = "Instructor";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(396, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Instructor";
             // 
             // label7
             // 
-            label7.AutoSize = true;
-            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label7.Location = new Point(45, 255);
-            label7.Name = "label7";
-            label7.Size = new Size(107, 20);
-            label7.TabIndex = 26;
-            label7.Text = "Requirements";
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(45, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 20);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Requirements";
             // 
             // label4
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(45, 162);
-            label4.Name = "label4";
-            label4.Size = new Size(89, 20);
-            label4.TabIndex = 27;
-            label4.Text = "Description";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(45, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Description";
             // 
             // label3
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(45, 44);
-            label3.Name = "label3";
-            label3.Size = new Size(51, 20);
-            label3.TabIndex = 28;
-            label3.Text = "Name";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(45, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Name";
             // 
-            // textBox3
+            // txtNametp
             // 
-            textBox3.Location = new Point(162, 44);
-            textBox3.Multiline = true;
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(499, 40);
-            textBox3.TabIndex = 33;
+            this.txtNametp.Location = new System.Drawing.Point(162, 44);
+            this.txtNametp.Multiline = true;
+            this.txtNametp.Name = "txtNametp";
+            this.txtNametp.Size = new System.Drawing.Size(499, 40);
+            this.txtNametp.TabIndex = 33;
             // 
-            // textBox4
+            // txtDescription
             // 
-            textBox4.Location = new Point(162, 162);
-            textBox4.Multiline = true;
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(499, 75);
-            textBox4.TabIndex = 33;
+            this.txtDescription.Location = new System.Drawing.Point(162, 162);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(499, 75);
+            this.txtDescription.TabIndex = 33;
             // 
-            // textBox5
+            // txtRequirements
             // 
-            textBox5.Location = new Point(162, 255);
-            textBox5.Multiline = true;
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(499, 90);
-            textBox5.TabIndex = 33;
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(490, 101);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(171, 28);
-            comboBox1.TabIndex = 35;
+            this.txtRequirements.Location = new System.Drawing.Point(162, 255);
+            this.txtRequirements.Multiline = true;
+            this.txtRequirements.Name = "txtRequirements";
+            this.txtRequirements.Size = new System.Drawing.Size(499, 90);
+            this.txtRequirements.TabIndex = 33;
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(45, 101);
-            label2.Name = "label2";
-            label2.Size = new Size(114, 20);
-            label2.TabIndex = 25;
-            label2.Text = "Type of Project";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(45, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Type of Project";
             // 
-            // comboBox2
+            // txtTypetp
             // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(162, 101);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(171, 28);
-            comboBox2.TabIndex = 35;
+            this.txtTypetp.Location = new System.Drawing.Point(165, 101);
+            this.txtTypetp.Name = "txtTypetp";
+            this.txtTypetp.Size = new System.Drawing.Size(166, 27);
+            this.txtTypetp.TabIndex = 36;
+            // 
+            // txtNameins
+            // 
+            this.txtNameins.Location = new System.Drawing.Point(481, 104);
+            this.txtNameins.Name = "txtNameins";
+            this.txtNameins.Size = new System.Drawing.Size(180, 27);
+            this.txtNameins.TabIndex = 37;
             // 
             // fTopicRegisterCreate
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(727, 595);
-            Controls.Add(comboBox2);
-            Controls.Add(comboBox1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox5);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox1);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(label11);
-            Controls.Add(label9);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(label7);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Name = "fTopicRegisterCreate";
-            Text = "fTopicRegisterCreate";
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 595);
+            this.Controls.Add(this.txtNameins);
+            this.Controls.Add(this.txtTypetp);
+            this.Controls.Add(this.txtStudent2);
+            this.Controls.Add(this.txtRequirements);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtNametp);
+            this.Controls.Add(this.txtStudent1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "fTopicRegisterCreate";
+            this.Text = "fTopicRegisterCreate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
 
-        private TextBox textBox2;
-        private TextBox textBox1;
+        private TextBox txtStudent2;
+        private TextBox txtStudent1;
         private Button button2;
-        private Button button1;
+        private Button btnCreate;
         private Label label11;
         private Label label9;
         private Label label1;
         private Label label7;
         private Label label4;
         private Label label3;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
-        private ComboBox comboBox1;
+        private TextBox txtNametp;
+        private TextBox txtDescription;
+        private TextBox txtRequirements;
         private Label label2;
-        private ComboBox comboBox2;
+        private TextBox txtTypetp;
+        private TextBox txtNameins;
     }
 }
--- fTopicRegisterCreate.cs
+++ fTopicRegisterCreate.cs
@@ -1,20 +1,42 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace FInalProject_Group06
 {
     public partial class fTopicRegisterCreate : Form
     {
+        SqlConnection conn = new SqlConnection(Properties.Settings.Default.connStr);
         public fTopicRegisterCreate()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TopicDAO topic = new TopicDAO();
+                if (topic.Addtopic(txtNametp.Text, txtTypetp.Text, txtNameins.Text, txtDescription.Text, txtRequirements.Text, txtStudent1.Text, txtStudent2.Text))
+                MessageBox.Show("Them thanh cong");
+        }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Them that bai" + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
