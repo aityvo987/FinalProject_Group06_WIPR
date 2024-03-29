bc7428d2d0c67769588c6572203ea81a609c6e7a
NTMT <nguyenthanhminhtriet3402@gmail.com>
2024-03-29 12:06:19 +0700

    test

--- obj/Debug/net6.0-windows/FInalProject_Group06.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/FInalProject_Group06.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FInalProject_Group06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64e5dff519f670337e1b4e956cd5e5bba35a476e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394b0722f575ead3bc9dee424d8028a49c9df138")]
 [assembly: System.Reflection.AssemblyProductAttribute("FInalProject_Group06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FInalProject_Group06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
